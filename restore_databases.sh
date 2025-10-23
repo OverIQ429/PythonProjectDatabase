2134f41ffdaeef07c9d8acf9d8a4c2fc6a91ea52
maksi <maksim.rylkin@mail.ru>
2025-10-24 00:00:01 +0300

    new cafe

--- restore_databases.sh
+++ restore_databases.sh
@@ -4,10 +4,10 @@ echo "🔄 Восстановление баз данных..."
 
 # Восстановление cafe_common
 echo "📦 Восстанавливаю cafe_common..."
-docker exec cafe_postgres_common psql -U postgres -d cafe_common -f /dumps/cafe_common.txt
+docker exec cafe_postgres_common psql -U postgres -d cafe_common -f /dumps/cafe_common
 
 # Восстановление cafe_branch_1
 echo "📦 Восстанавливаю cafe_branch_1..."
-docker exec cafe_postgres_branch psql -U postgres -d cafe_branch_1 -f /dumps/cafe_branch_1.txt
+docker exec cafe_postgres_branch psql -U postgres -d cafe_branch_1 -f /dumps/cafe_branch_1
 
 echo "✅ Базы данных восстановлены!"
